152933ace543958fffafd3bebb598192ee9e019a
ImperialGF <yakovfinagin93@mail.ru>
2025-09-07 15:00:51 +0300

    Project is created

---
+++ BMI.py
@@ -0,0 +1,13 @@
+weight = float(input("Введите свой вес в килограммах: "))
+height = float(input("Введите свой рост в метрах: "))
+
+bmi = round((weight / (height**2)), 1)
+
+if bmi < 18.5:
+    print(f"Ваш BMI: {bmi} (Недостаточный вес)")
+elif bmi <= 24.9:
+    print(f"Ваш BMI: {bmi} (Норма)")
+elif bmi <= 29.9:
+    print(f"Ваш BMI: {bmi} (Избыточный вес)")
+elif bmi >= 30:
+    print(f"Ваш BMI: {bmi} (Ожирение)")
